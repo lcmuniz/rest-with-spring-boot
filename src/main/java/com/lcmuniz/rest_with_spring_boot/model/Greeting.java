25eee391ebe211a80198e981073f6b933caf9fe2
Luiz Carlos Muniz <lcmuniz@gmail.com>
2026-01-12 14:23:08 -0300

    Create /greeting endpoint

---
+++ src/main/java/com/lcmuniz/rest_with_spring_boot/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.lcmuniz.rest_with_spring_boot.model;
+
+public record Greeting(Long id, String content) {
+}
---
+++ src/test/java/com/lcmuniz/rest_with_spring_boot/RestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcmuniz.rest_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
