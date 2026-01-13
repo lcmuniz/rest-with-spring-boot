db03683fe782d4090f33931c0148e5faa96dcaad
Luiz Carlos Muniz <lcmuniz@gmail.com>
2026-01-13 11:49:17 -0300

    add cors

--- src/main/java/com/lcmuniz/rest_with_spring_boot/config/WebConfig.java
+++ src/main/java/com/lcmuniz/rest_with_spring_boot/config/WebConfig.java
@@ -3,10 +3,20 @@ package com.lcmuniz.rest_with_spring_boot.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String allowedOrigins = "http://localhost:5173";
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
